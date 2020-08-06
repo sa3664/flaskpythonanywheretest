48f80ff2f80e7ff28c06601fafd05655cd371dd0
Shivani Aggarwal <sa3664@drexel.edu>
2020-08-06 18:39:30 -0400

    first commit

---
+++ DXC_Hack/Test.py
@@ -0,0 +1,19 @@
+
+from flask import Flask,redirect,url_for,render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html" , content = name, r=2 , list =["shivi","shad"])
+
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user",name ="Admin!"))
+
+if __name__ == "__main__":
+    app.run(debug= True )
