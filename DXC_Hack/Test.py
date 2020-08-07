9be138ed3e6c1c686f570212dd6a1a1d2674dae5
Shivani Aggarwal <sa3664@drexel.edu>
2020-08-07 16:38:08 -0400

    test

--- DXC_Hack/Test.py
+++ DXC_Hack/Test.py
@@ -1,19 +1,19 @@
 
-from flask import Flask,redirect,url_for,render_template
+# from flask import Flask,redirect,url_for,render_template
 
-app_test = Flask(__name__)
+# app_test = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
 
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello {name}!"s
+# # @app.route("/<name>")
+# # def user(name):
+# #     return f"Hello {name}!"s
 
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("user",name ="Admin!"))
+# # @app.route("/admin")
+# # def admin():
+# #     return redirect(url_for("user",name ="Admin!"))
 
-if __name__ == "__main__":
-    app_test.run(debug= True )
+# if __name__ == "__main__":
+#     app_test.run(debug= True )
