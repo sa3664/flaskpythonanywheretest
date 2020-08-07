8e5c84ae4a2180e37def42c6666c7734573d5ca4
Shivani Aggarwal <sa3664@drexel.edu>
2020-08-07 12:52:03 -0400

    new

--- DXC_Hack/Test_socketio.py
+++ DXC_Hack/Test_socketio.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask,redirect,url_for,render_template
 from flask_socketio import SocketIO, send
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='mysecret'
+app.config['SECRET_KEY'] =os.environ.get('SECRET')
 socketio= SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def home():
+    return render_template("index_socket_test.html")
 
 @socketio.on('message')
 def handleMessage(msg):
